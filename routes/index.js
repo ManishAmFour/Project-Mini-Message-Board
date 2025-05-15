b154c9dfc5f605c5f8cc331664fe9b6afcde31d8
ManishAmFour <ManishTewatia3020@gmail.com>
2025-05-16 01:19:17 +0530

    pushing files "Testing"

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,5 @@
 let express = require("express");
 let index = express.Router();
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date(),
-  },
-  {
-    text: "Hello World!",
-    user: "Charls",
-    added: new Date(),
-  },
-];
 
 const initiation = (req, res, next) => {
   console.log("home page initiated");
--- server.js
+++ server.js
@@ -1,12 +1,46 @@
 let express = require("express");
 let index = require("./routes/index");
 let newMessage = require("./routes/new-message");
+let path = require("path");
 
 const server = express();
 const PORT = 8000;
 
-server.use("/new", newMessage);
+server.set("view engine", "ejs");
+server.set("views", path.join(__dirname, "./views"));
 
-server.use("/", index);
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charls",
+    added: new Date(),
+  },
+];
+
+server.get("/", (req, res) => {
+  res.render("index", {
+    title: "Mini Messageboard",
+    messages: messages,
+    link: "/new",
+  });
+});
+
+server.get("/new", (req, res) => {
+  res.render("form", { title: "Mini Messageboard", messages: messages });
+});
+
+server.post("/new", express.urlencoded({ extended: true }), (req, res) => {
+  messages.push({
+    text: req.body.messageText,
+    user: req.body.authorsName,
+    added: new Date(),
+  });
+  res.redirect("/");
+});
 
 server.listen(PORT);
